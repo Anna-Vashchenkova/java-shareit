fd7f8701d5cb872df49a81d091f3c64120efc59d
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-02-02 00:16:10 +0300

    импорт

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.dto.ItemMapper;
 
 import java.util.List;
