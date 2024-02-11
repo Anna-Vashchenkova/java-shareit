e08df5f799e7d29ffe409a80dcb6e0421233d093
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-02-12 00:17:01 +0300

    Создание логов в итеме

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -21,6 +21,7 @@ public class ItemController {
 
     @GetMapping
     public List<ItemDto> get(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info("Получен запрос - показать список вещей пользователя '{}'", userId);
         return itemService.getItems(userId).stream()
                 .map(ItemMapper::toItemDto)
                 .collect(Collectors.toList());
@@ -29,7 +30,7 @@ public class ItemController {
     @PostMapping
     public ItemDto add(@RequestHeader("X-Sharer-User-Id") Long userId,
                     @RequestBody ItemDto dto) {
-
+        log.info("Получен запрос на добавление итема '{}' пользователю '{}'",dto, userId);
         return ItemMapper.toItemDto(itemService.addNewItem(
                 userId,
                 dto.getId(),
@@ -43,6 +44,7 @@ public class ItemController {
     public ItemDto updateItem(@PathVariable("itemId") long itemId,
                            @RequestHeader("X-Sharer-User-Id") Long userId,
                            @RequestBody ItemDto dto) {
+        log.info("Получен запрос на обновление данных итема '{}' у пользователя '{}'",itemId, userId);
         return ItemMapper.toItemDto(itemService.updateItem(
                 userId,
                 itemId,
@@ -54,18 +56,21 @@ public class ItemController {
 
     @GetMapping("/{itemId}")
     public ItemDto getItemById(@PathVariable("itemId") long itemId) {
+        log.info("Получен запрос - показать итем '{}'",itemId);
         return ItemMapper.toItemDto(itemService.getItemById(itemId));
     }
 
     @DeleteMapping("/{itemId}")
     public void deleteItem(@RequestHeader("X-Sharer-User-Id") long userId,
                            @PathVariable long itemId) {
+        log.info("Получен запрос на удаление итема '{}' пользователя '{}'",itemId, userId);
         itemService.deleteItem(userId, itemId);
     }
 
     @GetMapping("/search")
     public List<ItemDto> searchItem(@RequestHeader("X-Sharer-User-Id") long userId,
                               @RequestParam String text) {
+        log.info("Получен запрос на поиск итема по содержанию текста '{}' у пользователя '{}'",text, userId);
         return itemService.searchItem(text).stream()
                 .map(ItemMapper::toItemDto)
                 .collect(Collectors.toList());
