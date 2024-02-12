24e301dd81f7e52ad8c1636cf281efa36c1fa162
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-02-13 01:06:16 +0300

    Логирование user

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -20,26 +20,31 @@ public class UserController {
 
     @GetMapping
     public List<User> getAllUsers() {
+        log.info("Получен запрос на получение списка всех пользователей");
         return userService.getAllUsers();
     }
 
     @PostMapping
     public User saveNewUser(@RequestBody User user) {
+        log.info("Получен запрос на добавление пользователя '{}'", user);
         return userService.saveUser(user);
     }
 
     @PatchMapping("/{userId}")
     public User updateUser(@PathVariable("userId") Long userId, @RequestBody User user) {
+        log.info("Получен запрос на обновление данных пользователя '{}'", userId);
         return userService.updateUser(userId, user);
     }
 
     @GetMapping("/{userId}")
     public User getUser(@PathVariable("userId") Long userId) {
+        log.info("Получен запрос - показать данные пользователя '{}'", userId);
         return userService.getUserById(userId);
     }
 
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable("userId") Long userId) {
+        log.info("Получен запрос - удалить данные пользователя '{}'", userId);
          userService.deleteUserById(userId);
     }
 }
