51974aa3e91a61ba551deade5aaf38fd4e5e5ca7
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-03-03 12:17:33 +0300

    Тесты  ItemControllerTest

--- src/test/java/ru/practicum/shareit/item/ItemControllerMvcTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemControllerMvcTest.java
@@ -21,8 +21,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,15 +40,33 @@ class ItemControllerMvcTest {
     private User validUser2 = new User(2L, "bb@mail.ru", "Bb");
     private LocalDateTime created = LocalDateTime.of(2024, 02, 29, 12, 0, 0);
     private LocalDateTime created2 = LocalDateTime.of(2024, 3, 1, 14, 0, 0);
-    private LocalDateTime created3 = LocalDateTime.of(2024, 3, 1, 20, 0, 0);
+    private LocalDateTime created3 = LocalDateTime.of(2024, 3, 30, 20, 0, 0);
     private ItemRequest request1 = new ItemRequest(1L, "запрос1", validUser1, created);
     Item item3 = new Item(3L, "дрель", "ddd", Status.AVAILABLE, validUser2, request1);
     private ItemRequest request2 = new ItemRequest(2L, "запрос2", validUser1, created);
     Item item4 = new Item(4L, "дрель2", "ddd2", Status.AVAILABLE, validUser2, request2);
     private ItemRequest request3 = new ItemRequest(3L, "запрос3", validUser2, created);
-    Item item1 = new Item(1L, "перфоратор", "vvv", Status.AVAILABLE, validUser1, request3);
+    private Item item1 = new Item(1L, "перфоратор", "vvv", Status.AVAILABLE, validUser1, request3);
     private ItemRequest request4 = new ItemRequest(4L, "запрос4", validUser2, created);
-    Item item2 = new Item(2L, "перфоратор2", "vvv2", Status.AVAILABLE, validUser1, request4);
+    private Item item2 = new Item(2L, "перфоратор2", "vvv2", Status.AVAILABLE, validUser1, request4);
+    private Booking booking1 = new Booking(1L,
+            LocalDateTime.of(2024, 3, 1, 12, 0, 0),
+            LocalDateTime.of(2024, 3, 1, 13, 0, 0),
+            item1,
+            validUser2,
+            ru.practicum.shareit.booking.Status.APPROVED);
+    private Booking booking2 = new Booking(2L,
+            LocalDateTime.of(2024, 3, 30, 18, 0, 0),
+            LocalDateTime.of(2024, 3, 30, 19, 0, 0),
+            item1,
+            validUser2,
+            ru.practicum.shareit.booking.Status.APPROVED);
+    private Booking booking3 = new Booking(3L,
+            LocalDateTime.of(2024, 3, 30, 18, 0, 0),
+            LocalDateTime.of(2024, 3, 30, 19, 0, 0),
+            item2,
+            validUser2,
+            ru.practicum.shareit.booking.Status.APPROVED);
 
     @Test
     @DisplayName("При запросе должен вернуться список ItemOutcomeInfoDto")
@@ -97,13 +114,39 @@ class ItemControllerMvcTest {
     }
 
     @Test
+    @DisplayName("При запросе существующего пользователя должен вернуться ItemOutcomeDto")
+    void updateItem_whenUserFound_thenReturnItemOutcomeDto() throws Exception {
+        Mockito.when(this.itemService.updateItem(any(), any(), anyString(), anyString(), any())).thenReturn(item1);
+
+        mvc.perform(patch("/items/1")
+                        .header("X-Sharer-User-Id", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "  \"name\": \"перфоратор\",\n" +
+                                "  \"description\": \"vvv\",\n" +
+                                "  \"available\": true,\n" +
+                                "  \"requestId\": 3\n" +
+                                "}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.name").value("перфоратор"))
+                .andExpect(jsonPath("$.description").value("vvv"))
+                .andExpect(jsonPath("$.available").value("true"))
+                .andExpect(jsonPath("$.owner.id").value(1))
+                .andExpect(jsonPath("$.owner.email").value("aa@mail.ru"))
+                .andExpect(jsonPath("$.owner.name").value("Aa"))
+                .andExpect(jsonPath("$.requestId").value(3));
+    }
+
+    @Test
     @DisplayName("При запросе несуществующего пользователя должна появиться ошибка")
     void updateItem_whenUserNotFound_thenReturnValidationException() throws Exception {
         Mockito.when(this.itemService.updateItem(any(), any(), anyString(), anyString(), any()))
                 .thenThrow(new ValidationException(""));
 
-        mvc.perform(patch("/items/10")
-                .contentType(MediaType.APPLICATION_JSON)
+        mvc.perform(patch("/items/1")
+                        .header("X-Sharer-User-Id", 10)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content("{\n" +
                                 "  \"name\": \"перфоратор\",\n" +
                                 "  \"description\": \"vvv\",\n" +
@@ -117,28 +160,13 @@ class ItemControllerMvcTest {
     @DisplayName("При запросе должен вернуться ItemOutcomeInfoDto")
     void getItemById_shouldReturnItemOutcomeInfoDto() throws Exception {
         List<Booking> bookings = new ArrayList<>();
-        Booking booking1 = new Booking(1L,
-                LocalDateTime.of(2024, 3, 1, 12, 0, 0),
-                LocalDateTime.of(2024, 3, 1, 13, 0, 0),
-                item1,
-                validUser2,
-                ru.practicum.shareit.booking.Status.APPROVED
-                );
-        Booking booking2 = new Booking(2L,
-                LocalDateTime.of(2024, 3, 1, 18, 0, 0),
-                LocalDateTime.of(2024, 3, 1, 19, 0, 0),
-                item1,
-                validUser2,
-                ru.practicum.shareit.booking.Status.APPROVED
-        );
         bookings.add(booking1);
         bookings.add(booking2);
         List<Comment> comments = new ArrayList<>();
         Comment comment1 = new Comment(1L, "super", item1, validUser2, created2);
-        Comment comment2 = new Comment(2L, "super2", item1, validUser2, created3);
         comments.add(comment1);
-        comments.add(comment2);
 
+        Mockito.when(this.itemService.userIsOwnerOfItem(1L, 1L)).thenReturn(true);
         Mockito.when(this.bookingService.getBookingsForUser(1L)).thenReturn(bookings);
         Mockito.when(this.commentService.getComments(1L)).thenReturn(comments);
         Mockito.when(this.itemService.getItemById(1L, 1L)).thenReturn(item1);
@@ -162,31 +190,96 @@ class ItemControllerMvcTest {
                 .andExpect(jsonPath("$.lastBooking.end").value("2024-03-01T13:00:00"))
                 .andExpect(jsonPath("$.nextBooking.id").value(2))
                 .andExpect(jsonPath("$.nextBooking.bookerId").value(2))
-                .andExpect(jsonPath("$.nextBooking.start").value("2024-03-01T18:00:00"))
-                .andExpect(jsonPath("$.nextBooking.end").value("2024-03-01T19:00:00"))
+                .andExpect(jsonPath("$.nextBooking.start").value("2024-03-30T18:00:00"))
+                .andExpect(jsonPath("$.nextBooking.end").value("2024-03-30T19:00:00"))
                 .andExpect(jsonPath("$.comments.[0].id").value(1))
                 .andExpect(jsonPath("$.comments.[0].text").value("super"))
                 .andExpect(jsonPath("$.comments.[0].authorName").value("Bb"))
-                .andExpect(jsonPath("$.comments.[0].created").value("2024-03-01T14:00:00"))
-                .andExpect(jsonPath("$.comments.[1].id").value(2))
-                .andExpect(jsonPath("$.comments.[1].text").value("super2"))
-                .andExpect(jsonPath("$.comments.[1].authorName").value("Bb"))
-                .andExpect(jsonPath("$.comments.[1].created").value("2024-03-01T20:00:00"));
+                .andExpect(jsonPath("$.comments.[0].created").value("2024-03-01T14:00:00"));
     }
 
     @Test
     @DisplayName("При запросе ответ 200")
     void deleteItem() throws Exception{
         mvc.perform(
-                        delete("/items/1"))
+                        delete("/items/1")
+                                .header("X-Sharer-User-Id", 1))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void searchItem() {
+    @DisplayName("При запросе с валидными параметрами возвращается список ItemOutcomeDto")
+    void searchItem_whenRequestParamIsValid_thenReturnItemOutcomeDto() throws Exception {
+        int from = 0;
+        int size = 10;
+        String text = "перфоратор";
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+
+        Mockito.when(this.itemService.searchItem(text, from / size, size)).thenReturn(items);
+
+        mvc.perform(get("/items/search")
+                        .header("X-Sharer-User-Id", 1)
+                        .param("text", "перфоратор")
+                        .param("from", "0")
+                        .param("size", "10")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id").value(1))
+                .andExpect(jsonPath("$.[0].name").value("перфоратор"))
+                .andExpect(jsonPath("$.[0].description").value("vvv"))
+                .andExpect(jsonPath("$.[0].available").value("true"))
+                .andExpect(jsonPath("$.[0].owner.id").value(1))
+                .andExpect(jsonPath("$.[0].owner.email").value("aa@mail.ru"))
+                .andExpect(jsonPath("$.[0].owner.name").value("Aa"))
+                .andExpect(jsonPath("$.[0].requestId").value(3))
+                .andExpect(jsonPath("$.[1].id").value(2))
+                .andExpect(jsonPath("$.[1].name").value("перфоратор2"))
+                .andExpect(jsonPath("$.[1].description").value("vvv2"))
+                .andExpect(jsonPath("$.[1].available").value("true"))
+                .andExpect(jsonPath("$.[1].owner.id").value(1))
+                .andExpect(jsonPath("$.[1].owner.email").value("aa@mail.ru"))
+                .andExpect(jsonPath("$.[1].owner.name").value("Aa"))
+                .andExpect(jsonPath("$.[1].requestId").value(4));
     }
 
     @Test
-    void addComment() {
+    @DisplayName("При запросе с невалидными параметрами from < 0, size < 1 возвращается ошибка")
+    void searchItem_whenRequestParamIsNotValid_thenReturnValidationException() throws Exception {
+        String text = "перфоратор";
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+
+        Mockito.when(this.itemService.searchItem(eq(text), anyInt(), anyInt()))
+                .thenThrow(new ValidationException("Неверные параметры запроса"));
+
+        mvc.perform(get("/items/search")
+                        .header("X-Sharer-User-Id", 1)
+                        .param("text", "перфоратор")
+                        .param("from", "-1")
+                        .param("size", "0")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(400));
+    }
+
+    @Test
+    @DisplayName("При запросе возвращается CommentDto")
+    void addComment_shouldReturnCommentDto() throws Exception {
+        Comment comment1 = new Comment(1L, "super", item1, validUser2, created2);
+        Mockito.when(this.commentService.addComment(2L, 1L, "super")).thenReturn(comment1);
+
+        mvc.perform(post("/items/1/comment")
+                        .header("X-Sharer-User-Id", 2)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "  \"text\": \"super\"\n" +
+                                "}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.text").value("super"))
+                .andExpect(jsonPath("$.authorName").value("Bb"))
+                .andExpect(jsonPath("$.created").value("2024-03-01T14:00:00"));
     }
 }
