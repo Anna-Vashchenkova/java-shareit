dd9428d5f66036857d31acfa332fd4cd9798f4fe
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-03-03 12:18:37 +0300

    Тесты  ItemControllerTest импорты

--- src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemControllerTest.java
@@ -25,8 +25,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
