d85d502a0e821c178fad93de020b86eb9ea2bdfa
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-03-03 22:40:35 +0300

    Тесты выполнены - ItemServiceImplTest

--- src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import ru.practicum.shareit.booking.Booking;
@@ -22,9 +19,9 @@ import ru.practicum.shareit.user.model.User;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,12 +42,7 @@ class ItemServiceImplTest {
     private ItemRequest request2 = new ItemRequest(2L, "запрос2", validUser2, created);
     private Item item1 = new Item(1L, "перфоратор", "vvv", Status.AVAILABLE, validUser1, request1);
     private Item item2 = new Item(2L, "перфоратор2", "vvv2", Status.AVAILABLE, validUser1, request2);
-    private Booking booking1 = new Booking(1L,
-            LocalDateTime.of(2024, 3, 1, 12, 0, 0),
-            LocalDateTime.of(2024, 3, 1, 13, 0, 0),
-            item1,
-            validUser2,
-            ru.practicum.shareit.booking.Status.APPROVED);
+    @Captor
     private ArgumentCaptor<Item> itemCaptor;
 
     @Test
@@ -159,26 +151,118 @@ class ItemServiceImplTest {
         Mockito.when(repository.getById(1L)).thenReturn(itemUnavailable);
 
         itemService.updateItem(1L, 1L, "перфоратор", "vvv", available);
-        Assertions.assertEquals(itemUnavailable, item1);
+
+        verify(repository).save(itemCaptor.capture());
+        Assertions.assertEquals(Status.UNAVAILABLE, itemCaptor.getValue().getAvailable());
     }
 
     @Test
-    void getItemById() {
+    @DisplayName("При валидных параметрах вернуть Item")
+    void getItemById_whenParamIsValid_thenReturnItem() {
+        Mockito.when(userService.getUserById(1L)).thenReturn(validUser1);
+        Mockito.when(repository.findById(1L)).thenReturn(Optional.of(item1));
+
+        Item result = itemService.getItemById(1L, 1L);
+
+        Assertions.assertEquals(item1, result);
     }
 
     @Test
-    void findItemsByOwnerId() {
+    @DisplayName("При значении пользователя null вернуть DataNotFoundException")
+    void getItemById_whenUserNotFound_thenReturnDataNotFoundException() {
+        Mockito.when(userService.getUserById(10L)).thenReturn(null);
+
+        DataNotFoundException exception = Assertions.assertThrows(DataNotFoundException.class,
+                () -> itemService.getItemById(10L, 1L));
+
+        Assertions.assertEquals("Пользователь не найден.", exception.getMessage());
     }
 
     @Test
-    void searchItem() {
+    @DisplayName("При значении item =  null вернуть DataNotFoundException")
+    void getItemById_whenItemNotFound_thenReturnDataNotFoundException() {
+        Mockito.when(userService.getUserById(1L)).thenReturn(validUser1);
+        Mockito.when(repository.findById(10L)).thenReturn(Optional.empty());
+
+        DataNotFoundException exception = Assertions.assertThrows(DataNotFoundException.class,
+                () -> itemService.getItemById(1L, 10L));
+
+        Assertions.assertEquals("Вещь с таким id не найдена.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Успешное получение списка итемов по id владельца")
+    void findItemsByOwnerId_whenUserFound_thenReturnItems() {
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+        Mockito.when(userService.getUserById(1L)).thenReturn(validUser1);
+        Mockito.when(repository.findByOwnerId(1L)).thenReturn(items);
+
+        List<Item> result = itemService.findItemsByOwnerId(1L);
+
+        Assertions.assertEquals(2, result.size());
+    }
+
+    @Test
+    @DisplayName("При значении пользователя null вернуть DataNotFoundException")
+    void findItemsByOwnerId_whenUserNotFound_thenReturnDataNotFoundException() {
+        Mockito.when(userService.getUserById(10L)).thenReturn(null);
+
+        DataNotFoundException exception = Assertions.assertThrows(DataNotFoundException.class,
+                () -> itemService.findItemsByOwnerId(10L));
+
+        Assertions.assertEquals("Владелец вещи не найден", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Успешное получение списка итемов при вылидных параметрах метода")
+    void searchItem_whenParamIsValid_thenReturnItems() {
+        String text = "перфоратор";
+        int from = 0;
+        int size = 10;
+        Page mockPage = Mockito.mock(Page.class);
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+        Mockito.when(mockPage.getContent()).thenReturn(items);
+        Mockito.when(repository.searchItem(eq(text), any())).thenReturn(mockPage);
+
+        List<Item> result = itemService.searchItem(text, from, size);
+
+        Assertions.assertEquals(2, result.size());
+
+    }
+
+    @Test
+    @DisplayName("При поиску по пустой строке вернуть пустой список итомов")
+    void searchItem_whenTextIsEmpty_thenReturnEmptyList() {
+        String text = "";
+        int from = 0;
+        int size = 10;
+
+        List<Item> result = itemService.searchItem(text, from, size);
+        Assertions.assertEquals(0, result.size());
     }
 
     @Test
     void userIsOwnerOfItem() {
+        Mockito.when(userService.getUserById(1L)).thenReturn(validUser1);
+        Mockito.when(repository.getReferenceById(1L)).thenReturn(item1);
+
+        boolean result = itemService.userIsOwnerOfItem(1L, 1L);
+        Assertions.assertTrue(result);
     }
 
     @Test
-    void findItemsByRequestId() {
+    @DisplayName("Вернуть список итемов по id запроса")
+    void findItemsByRequestId_shouldReturnItems() {
+        List<Item> items = new ArrayList<>();
+        items.add(item1);
+        Mockito.when(repository.findAllByRequestId(anyLong())).thenReturn(items);
+
+        List<Item> itemsByRequestId = itemService.findItemsByRequestId(1L);
+
+        Assertions.assertEquals(1, itemsByRequestId.size());
     }
 }
