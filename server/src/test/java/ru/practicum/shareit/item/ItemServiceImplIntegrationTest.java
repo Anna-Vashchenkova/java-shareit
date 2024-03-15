66c2aa961261397c47a22643f653bf87d179d5bf
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-03-15 12:42:47 +0300

    Check test pom

--- server/src/test/java/ru/practicum/shareit/item/ItemServiceImplIntegrationTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemServiceImplIntegrationTest.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -113,7 +112,6 @@ class ItemServiceImplIntegrationTest {
     }
 
     @Test
-    @Disabled
     void addNewItem() {
         userRepository.save(owner);
         Item itemExpcted = itemService.addNewItem(owner.getId(), "item1", "description1", true, null);
