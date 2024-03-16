417e89c24af0baf02f2df75fe98b6b43e5ef6eff
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2024-03-17 01:19:02 +0300

    Исправление замечаний по ревью- ItemRequestController, UserController

--- gateway/src/main/java/ru/practicum/shareit/gateway/user/controller/UserController.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/user/controller/UserController.java
@@ -21,62 +21,60 @@ import java.util.List;
 @Validated
 public class UserController {
     private final WebClient webClient;
+    private static final String API_PREFIX = "/users";
+
 
     @GetMapping
-    public List<UserDto> getAllUsers() {
+    public Mono<List<UserDto>> getAllUsers() {
         log.info("Получен запрос на получение списка всех пользователей");
-        Mono<List<UserDto>> response = webClient.get()
-                .uri(uriBuilder -> uriBuilder.path("/users").build())
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(API_PREFIX).build())
                 .retrieve()
                 .bodyToMono(new ParameterizedTypeReference<List<UserDto>>() {});
-        return response.block();
     }
 
     @PostMapping
-    public UserDto saveNewUser(@Valid @RequestBody UserDto dto) {
+    public Mono<UserDto> saveNewUser(@Valid @RequestBody UserDto dto) {
         log.info("Получен запрос на добавление пользователя '{}'", dto);
-        Mono<UserDto> userDtoMono = webClient.post()
-                .uri("/users")
+        return webClient.post()
+                .uri(API_PREFIX)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(dto)
                 .retrieve()
                 .bodyToMono(UserDto.class);
-        return userDtoMono.block();
     }
 
     @PatchMapping("/{userId}")
-    public UserDto updateUser(@PathVariable("userId") Long userId, @RequestBody UserDto dto) {
+    public Mono<UserDto> updateUser(@PathVariable("userId") Long userId, @RequestBody UserDto dto) {
         log.info("Получен запрос на обновление данных пользователя '{}'", userId);
         if (dto.getId() == null) {
             dto.setId(userId);
         }
-        Mono<UserDto> userDtoMono = webClient.patch()
-                .uri("/users/{userId}", userId)
+        return webClient.patch()
+                .uri(API_PREFIX + "/{userId}", userId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(dto)
                 .retrieve()
                 .bodyToMono(UserDto.class);
-        return userDtoMono.block();
     }
 
     @GetMapping("/{userId}")
-    public UserDto getUser(@PathVariable("userId") Long userId) {
+    public Mono<UserDto> getUser(@PathVariable("userId") Long userId) {
         log.info("Получен запрос - показать данные пользователя '{}'", userId);
-        Mono<UserDto> userDtoMono = webClient.get()
-                .uri("/users/{userId}", userId)
+        return webClient.get()
+                .uri(API_PREFIX + "/{userId}", userId)
                 .retrieve()
                 .onStatus(httpStatus -> httpStatus.is4xxClientError(),
                         clientResponse -> Mono.error(new DataNotFoundException("Пользователь не найден")))
                 .bodyToMono(UserDto.class);
-        return userDtoMono.block();
     }
 
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable("userId") Long userId) {
         log.info("Получен запрос - удалить данные пользователя '{}'", userId);
         webClient.delete()
-                .uri("/users/{userId}", userId)
+                .uri(API_PREFIX + "/{userId}", userId)
                 .retrieve()
-                .bodyToMono(Void.class).block();
+                .bodyToMono(Void.class);
     }
 }
